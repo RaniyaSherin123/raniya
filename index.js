3ad3210ad6109524bd07e0c86f0f861740815c4b
Raniya Sherin <2020.cse.raniya@ekc.edu.in>
2024-08-26 14:21:29 +0530

    navbar

---
+++ index.js
@@ -0,0 +1,16 @@
+// let str = 'abcde';
+// let res = [];
+// let temp = '';
+// for (let i = 0; i < str.length; i++) {
+//     if (i == str.length - 1) {
+//         temp = str[i];
+//         res.push(temp);
+//     }
+//     else {
+//         temp = str[i] + str[ + 1];
+//         res.push(temp)
+//     }
+// }
+// console.log(res);
+
+// out = ['ab','bc','cd','de','e']
